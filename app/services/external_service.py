638d0a94761c607a4dd938c2bad906e12dd5e4d8
Hiroaki <oncwnuDzloy2ChjzYOTh5H0SW4B0@git.weixin.qq.com>
2024-09-28 22:02:45 +0800

    COS函数小改

--- app/services/external_service.py
+++ app/services/external_service.py
@@ -1,5 +1,5 @@
+import os
 import time
-
 from qcloud_cos import CosConfig, CosS3Client
 from config.base import Config
 
@@ -29,8 +29,12 @@ def handle_file_upload(file_type, file, user_id):
     if directory is None:
         return {"error": "Invalid file type"}, 400
 
-    # 使用用户 ID 和时间戳生成唯一文件名
-    file_name = f"{user_id}_{int(time.time())}_{file.filename}"
+    # 头像时使用用户 ID 作为文件名，其他类型保留原文件名并加上时间戳
+    if file_type == "avatar":
+        file_name = f"{user_id}{os.path.splitext(file.filename)[-1]}"  # 保留文件扩展名
+    else:
+        file_name = f"{user_id}_{int(time.time())}_{file.filename}"
+
     object_key = f"{directory}/{file_name}"
 
     try:
@@ -38,10 +42,6 @@ def handle_file_upload(file_type, file, user_id):
             Bucket=Config.COS_BUCKET_NAME,
             Body=file,
             Key=object_key,
-            # StorageClass='STANDARD',
-            # Header={
-            #     "x-cos-meta-user-id": user_id,  # 添加自定义请求头
-            # }
         )
         return {"message": "File uploaded successfully", "ETag": response['ETag'], "file_path": object_key}, 200
     except Exception as e:
